80ee56bf1395e2001a78c94f6890d89977918d9d
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-02-19 06:08:42 -0800

    imports

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
+//config
 const express = require('express');
 const app = express();
 const port = 4000 || process.env.PORT;
+const graphqlHTTP = require('express-graphql');
 
 app.listen(port, () => {
 	console.log(`Node + GraphQL server is running.`);
