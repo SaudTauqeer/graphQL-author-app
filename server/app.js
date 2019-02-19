cb9b823c3a844d96374fa545a9e07d572b6954b0
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-02-19 06:12:36 -0800

    added graphl middleware

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 4000 || process.env.PORT;
 const graphqlHTTP = require('express-graphql');
 
+//graphql-express middleware
+app.use('/graphql', graphqlHTTP({}));
+
 app.listen(port, () => {
 	console.log(`Node + GraphQL server is running.`);
 });
