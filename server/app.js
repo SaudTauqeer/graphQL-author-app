d88618948225ff6601543d0b09351eaff3ce9e26
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-02-24 10:48:19 -0800

    migrated back to js, typescript overkill

--- server/app.js
+++ server/app.js
@@ -1,18 +1,12 @@
 //config
-import express from 'express';
+const express = require('express');
 const app = express();
 const port = 4000 || process.env.PORT;
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
 
 //graphql-express middleware
-app.use(
-	'/graphql',
-	graphqlHTTP({
-		schema
-	})
-);
+app.use('/graphql', graphqlHTTP({}));
 
-app.listen(port, (): void => {
+app.listen(port, () => {
 	console.log(`Node + GraphQL server is running at http://localhost:${port}`);
 });
