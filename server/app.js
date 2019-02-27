11654513aff9f4c1157ee3533c8d1ad07a3757bd
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-02-27 07:26:20 -0800

    added author type

--- server/app.js
+++ server/app.js
@@ -3,9 +3,16 @@ const express = require('express');
 const app = express();
 const port = 4000 || process.env.PORT;
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 //graphql-express middleware
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
 
 app.listen(port, () => {
 	console.log(`Node + GraphQL server is running at http://localhost:${port}`);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,35 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
 
 // dummy data
-var books = [
+const books = [
 	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
 	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
 	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
 ];
 
+const authors = [
+	{ name: 'Patrick sans', age: 34, id: 1 },
+	{ name: 'Sans undertale', age: 34, id: 2 },
+	{ name: 'Mr obama', age: 89, id: 3 }
+];
+
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString }
+	})
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLID },
+		age: { type: GraphQLInt }
 	})
 });
 
@@ -23,10 +38,15 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		book: {
 			type: BookType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				return books.find((books) => books.id === args.id);
 			}
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {}
 		}
 	}
 });
